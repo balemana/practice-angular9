c1aeab6cedbdb58bf6dfe3f8011445ddb5536b95
balemana <balemana@startechup.com>
2020-10-29 16:20:53 +0800

    Add dynamic class with ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-server',
-  templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+        p {
+          padding: 5px 10px;
+        }
+    `]
 })
-export class ServerComponent implements OnInit {
+export class ServerComponent {
+    serverId: number = 10;
+    serverStatus: string = 'offline';
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+    }
+    getServerStatus() {
+        return this.serverStatus;
+    }
+    getColor() {
+        return this.serverStatus === 'online' ? 'green': 'red';
+    }
 }
